535974c25184e8ac5aefc99ac952b53b86fe0c21
Maria Velazquez <meryvel@gmail.com>
2020-09-01 08:57:51 -0300

    first commit

---
+++ mern_cliente/src/components/proyectos/Proyecto.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Proyecto = ({proyecto}) => {
+    return (  
+        <li>
+            <button
+                type="button"
+                className="btn btn-blank"            
+            >{proyecto.name}</button>
+        </li>
+        
+    );
+}
+ 
+export default Proyecto;
